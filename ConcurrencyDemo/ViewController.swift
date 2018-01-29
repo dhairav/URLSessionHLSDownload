e40d732f080a7443ecdcc77c062f3f9c8e566d5b
Dhairav Mehta <dhairav.mehta@sugarboxnetworks.com>
2018-01-29 21:05:36 +0530

    Core - Added functionality to correctly download, playback, pause and resume downloads while app is in foreground

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -12,40 +12,81 @@ import AVKit
 import GCDWebServer
 
 let builder = ManifestBuilder()
-let masterUrl = URL(string: "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8")
-let manifest = builder.parse(masterUrl!)
-// Get local file path: download task stores tune here; AV player plays it.
+var masterUrl = URL(string: "http://drm01.sboxdc.com/sample_media/hls_enc/master.m3u8")
+var manifest = builder.parse(masterUrl!)
 let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 var localDownloadUrl = documentsPath.appendingPathComponent("Sample", isDirectory: true)
 var completedDownloadsCount = 0
 var totalSegmentCount: Int? = nil
-var canPlayBack = true
+var canPlayBack = false
+var playlist: MediaPlaylist? = nil
 var session: URLSession? = nil
-
+var path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
 let sessionConfig = URLSessionConfiguration.background(withIdentifier: "InitialDownload")
-
+var totalDownloadedBytes: Int64 = 0
+var isDownloading = false
 
 class ViewController: UIViewController, URLSessionTaskDelegate, URLSessionDownloadDelegate {
+    
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var pauseButton: UIButton!
+    
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         
         let segmentFilePath = localDownloadUrl.appendingPathComponent((downloadTask.response?.url?.lastPathComponent)!)
         
         do {
-            try FileManager.default.moveItem(at: location, to: segmentFilePath)
-            debugPrint("Moved file: \(String(describing: downloadTask.response?.url?.lastPathComponent))")
+            if FileManager.default.fileExists(atPath: segmentFilePath.absoluteString) {
+                try FileManager.default.removeItem(at: segmentFilePath)
+                try FileManager.default.moveItem(at: location, to: segmentFilePath)
+                debugPrint("Replaced file: \(String(describing: downloadTask.response?.url?.lastPathComponent))")
+            }
+            else {
+                try FileManager.default.moveItem(at: location, to: segmentFilePath)
+                debugPrint("Moved file: \(String(describing: downloadTask.response?.url?.lastPathComponent))")
+            }
+            if (downloadTask.response?.url?.lastPathComponent == masterUrl?.lastPathComponent) {
+                var selectedPlaylistUrl = masterUrl?.deletingLastPathComponent().absoluteURL
+                
+                selectedPlaylistUrl = selectedPlaylistUrl?.appendingPathComponent((manifest.getPlaylist(1)?.path)!)
+                playlist = manifest.getPlaylist(1)
+                let playlistRequest = URLRequest(url: selectedPlaylistUrl!)
+                let playlistDownloadTask = session.downloadTask(with: playlistRequest)
+                playlistDownloadTask.resume()
+                startDownloadingSegments()
+            }
             completedDownloadsCount += 1
         }catch{
             print(error.localizedDescription)
+            if error.localizedDescription.contains("already exists") {
+                
+            }
         }
         if(completedDownloadsCount == totalSegmentCount) {
             canPlayBack = true
+            print("Downloading crypt key now, all segments downloaded.")
+            let cryptTask = session.downloadTask(with: (masterUrl?.deletingLastPathComponent().appendingPathComponent("crypt.key"))!)
+            cryptTask.resume()
+            isDownloading = false
         }
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         if totalBytesExpectedToWrite > 0 {
-            let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-            debugPrint("Progress \(String(describing: downloadTask.response?.url?.lastPathComponent)) \(progress * 100)%")
+            
+            totalDownloadedBytes += bytesWritten
+
+            let progress = Float(totalDownloadedBytes) / Float(261503000)
+            
+//            debugPrint("Progress \(String(describing: downloadTask.response?.url?.lastPathComponent)) \(progress * 100)%")
+            
+            DispatchQueue.main.async {
+                self.progressView.setProgress(Float(progress), animated: true)
+                let progressToDisplay = (progress * 100).rounded()
+                self.progressLabel.text = "progress: \(progressToDisplay)%"
+                
+            }
         }
     }
     
@@ -53,72 +94,122 @@ class ViewController: UIViewController, URLSessionTaskDelegate, URLSessionDownlo
         debugPrint("Task completed: \(task), error: \(String(describing: error))")
     }
     
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        sessionConfig.allowsCellularAccess = false
-        sessionConfig.httpMaximumConnectionsPerHost = 5
-        session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue())
-        
-        
-        let server = GCDWebServer()
-        server.addGETHandler(forBasePath: "/", directoryPath: documentsPath.absoluteString, indexFilename: "index.html", cacheAge: 3600, allowRangeRequests: true)
-        server.start(withPort: 3000, bonjourName: nil)
-        print(server.serverURL)
-        print(documentsPath.absoluteString)
-        
-    }
-
-    @IBAction func didClickOnStart(sender: AnyObject) {
-        
-        do {
-            try FileManager.default.createDirectory(atPath: localDownloadUrl.path, withIntermediateDirectories: true, attributes: nil)
-        }catch {
-            print(error.localizedDescription)
+    @IBAction func didClickOnPause(_ sender: Any) {
+        if isDownloading {
+           
+            session?.getAllTasks(completionHandler: { (tasks) in
+                for task in tasks {
+                    task.suspend()
+                }
+            })
+            isDownloading = false
+            canPlayBack = false
+            pauseButton.setTitle("Resume", for: .normal)
+        }else if canPlayBack {
+            let alert = UIAlertController(title: "Alert", message: "Content Already downnloaded", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if !canPlayBack && (pauseButton.currentTitle?.contains("Resume"))! {
+            session?.getAllTasks(completionHandler: { (tasks) in
+                for task in tasks {
+                    task.resume()
+                }
+            })
+            isDownloading = true
+            canPlayBack = false
+            pauseButton.setTitle("Pause", for: .normal)
         }
-        let mediaPlaylistSample = manifest.getPlaylist(0)
+    }
+    
+    func startDownloadingSegments() {
+        isDownloading = true
         
-        totalSegmentCount = mediaPlaylistSample?.getSegmentCount()
-
-        for i in 0..<mediaPlaylistSample!.getSegmentCount() {
-
-            let tsName = mediaPlaylistSample?.getSegment(i)?.path
-
+        totalSegmentCount = playlist?.getSegmentCount()
+        
+        for i in 0..<totalSegmentCount! {
+            
+            let tsName = playlist?.getSegment(i)?.path
+            
             let components = masterUrl?.absoluteString.components(separatedBy: "/")
             let stringToRemove = components?[(components?.count)!-1]
-
+            
             let downloadUrl = masterUrl?.absoluteString.replacingOccurrences(of: stringToRemove!, with:tsName!)
             print(downloadUrl!)
             let request = URLRequest(url: URL.init(string: downloadUrl!)!)
             let downloadTask = session?.downloadTask(with: request)
             downloadTask?.resume()
-
         }
+    }
     
-        let request = URLRequest(url: masterUrl!)
-        let downloadTask = session?.downloadTask(with: request)
-        downloadTask?.resume()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let masterString = try! String.init(contentsOf: localDownloadUrl.appendingPathComponent("master.m3u8"), encoding: .utf8)
+//
+//        let masterStringArray: [String] = masterString.components(separatedBy: CharacterSet.newlines)
+//
+//        var newMasterString: String = ""
+//
+//        var copyNext: Bool = false
+//        for line in masterStringArray {
+//            if (copyNext == true) {
+//                newMasterString.append("\(line)\n")
+//                copyNext = false
+//            }
+//            else if (line.starts(with: "#EXT-X-STREAM-INF") && line.contains("720")) {
+//                newMasterString.append("\(line)\n");
+//                copyNext = true
+//            }
+//            else {
+//                if line.starts(with: "#") && !line.contains("RESOLUTION") {
+//                    newMasterString.append("\(line)\n")
+//                }
+//            }
+//        }
+//
+//        do {
+//            try FileManager.default.removeItem(at: localDownloadUrl.appendingPathComponent("master.m3u8"))
+//
+//            try newMasterString.write(toFile: localDownloadUrl.appendingPathComponent("master.m3u8").path, atomically: true, encoding: .utf8)
+//
+//        }catch {
+//            print(error.localizedDescription)
+//        }
+//
         
-        var playListUrl = masterUrl?.deletingLastPathComponent().absoluteURL
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func didClickOnStart(sender: AnyObject) {
         
-        playListUrl = playListUrl?.appendingPathComponent((mediaPlaylistSample?.path)!)
+        sessionConfig.allowsCellularAccess = false
+        sessionConfig.httpMaximumConnectionsPerHost = 5
+        session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue())
         
-        let playlistRequest = URLRequest(url: playListUrl!)
-        let playlistDownloadTask = session?.downloadTask(with: playlistRequest)
-        playlistDownloadTask?.resume()
+        do {
+            try FileManager.default.createDirectory(atPath: localDownloadUrl.path, withIntermediateDirectories: true, attributes: nil)
+            let task = session?.downloadTask(with: masterUrl!)
+            task?.resume()
+        }catch {
+            print(error.localizedDescription)
+        }
+        
     }
     
     @IBAction func playContent(_ sender: Any) {
         
         if canPlayBack {
-
-            let playbackUrl = URL.init(string: "http://localhost:3000/playlist.m3u8")
-//            let playbackUrl = localDownloadUrl.appendingPathComponent("chunklist_w1792260766_b560000.m3u8")
+            
+            let server = GCDWebServer()
+            server.addGETHandler(forBasePath: "/", directoryPath: path, indexFilename: nil, cacheAge: 3600, allowRangeRequests: true)
+            server.start(withPort: 3000, bonjourName: nil)
+            
+            let playbackUrl = URL.init(string: "http://localhost:3000/Sample/master.m3u8")
             let player = AVPlayer.init(url: playbackUrl!)
             let playerController = AVPlayerViewController()
             playerController.player = player

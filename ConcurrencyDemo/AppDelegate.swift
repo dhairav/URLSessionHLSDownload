a9ad37ceaf154f73daf2fbe240f34c4d9e4764e1
Dhairav Mehta <dhairav.mehta@sugarboxnetworks.com>
2018-01-29 21:04:45 +0530

    Core - Checking in AppDelegate if GCD Web server is running, if it is, stopping it when app goes in background

--- ConcurrencyDemo/AppDelegate.swift
+++ ConcurrencyDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GCDWebServer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -25,6 +26,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        let server = GCDWebServer()
+        if server.isRunning {
+            server.stop()
+        }
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -40,6 +45,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 }
 
